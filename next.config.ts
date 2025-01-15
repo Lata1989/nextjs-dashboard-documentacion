9f01effd283c11b01bfd1da07762483a8d648534
Lata1989 <pabloalejandrodelaiglesia@gmail.com>
2025-01-15 00:27:20 -0300

    capitulo 11 listo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
+
+
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
