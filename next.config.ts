9b05a89ea260d1ff5d4e0d94eeb2902f056680cc
Lata1989 <pabloalejandrodelaiglesia@gmail.com>
2025-01-15 20:47:03 -0300

    actualizado

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   // experimental: {
   //   ppr: 'incremental',
   // },
+  eslint: {
+    ignoreDuringBuilds: true, // Desactiva ESLint en la construcción
+  },
 };
 
 export default nextConfig;
-
 
 // import type { NextConfig } from 'next';
 
